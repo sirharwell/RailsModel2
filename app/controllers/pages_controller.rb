0250c899a4aab3d8bfc6db61c4c258fa24fc28ed
Ian Harwell <sirharwell@gmail.com>
2018-02-27 17:23:05 -0700

    delete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -19,6 +19,25 @@ class PagesController < ApplicationController
   else
     render :new
   end
+
+  def edit
+  @page = Page.find(params[:id])
+  end
+
+  def update
+  @page = Page.find(params[:id])
+
+  if @page.update(page_params)
+    redirect_to pages_path
+  else
+    render :edit
+  end
+end
+
+def destroy
+  Page.find(params[:id]).destroy
+  redirect_to pages_path
+end
 end
 
 private
